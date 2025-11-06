15e88b6203404ef51a5c05583e3458d078b39813
Maria Fernanda <mariaferpinguelli@hotmail.com>
2025-11-06 17:14:43 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 from modules.graph_reader import read_and_format_data
-from modules.eccentricity import calculate_eccentricity
-from modules.closeness_centrality import calculate_closeness_centrality
+from modules.betweenness import calculate_betweenness_centrality
 from modules.print_results import print_results
 
 def main():
     # Tratar dados de entrada
     graph = read_and_format_data()
 
-    # Calcular excentricidade do grafo
-    calculate_eccentricity(graph)
-
-    # Calcular closeness centrality dos vértices
-    calculate_closeness_centrality(graph)
+    # Calcular betweenness centrality dos vértices
+    calculate_betweenness_centrality(graph)
 
     # Imprime resultados no terminal, e guarda em arquivos txt
     print_results(graph)
