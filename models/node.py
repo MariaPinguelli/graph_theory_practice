9c3a72d5ec28518fa5fd0855c6f4c9642578e3ef
Maria Fernanda <mariaferpinguelli@hotmail.com>
2025-11-06 17:13:41 -0300

    Add files via upload

--- models/node.py
+++ models/node.py
@@ -3,6 +3,5 @@ class Node:
     def __init__(self, id, label):
         self.id = id
         self.label = label
-        self.eccentricity = None
+        self.betweenness = None
         self.distances = None
-        self.closeness = None
